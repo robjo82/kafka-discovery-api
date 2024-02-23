10daf8654c24a00e957aaa9fdc01e562ad462a80
Robin JOSEPH <robin.joseph@decathlon.com>
2024-02-23 11:14:51 +0100

    first commit

---
+++ src/main/java/com/kafkadiscovery/api/KafkaDiscoveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.kafkadiscovery.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDiscoveryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDiscoveryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafkadiscovery/api/KafkaDiscoveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkadiscovery.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDiscoveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
