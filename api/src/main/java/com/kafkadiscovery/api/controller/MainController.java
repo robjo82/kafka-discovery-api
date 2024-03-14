0261f47279de5234d903c0aa0caa238a91bea6e8
Robin JOSEPH <robin.joseph@decathlon.com>
2024-03-14 19:55:24 +0100

    Add Mirth Connect service and update API controller
    Merge is successful!
    
    This update introduces a Mirth Connect service to the docker-compose file. The API controller has been revised to receive XML data in the request body. Also, the application port has been changed from 8182 to 8082, and a backup configuration XML for Mirth Connect is added.

--- api/src/main/java/com/kafkadiscovery/api/controller/MainController.java
+++ api/src/main/java/com/kafkadiscovery/api/controller/MainController.java
@@ -18,8 +18,24 @@ public class MainController {
     private final KafkaSender kafkaSender;
      
    @PostMapping("/data")
-   public ResponseEntity<String> uploadFile(@RequestParam("message") String message) {
-       kafkaSender.send("topic1", message);
-       return ResponseEntity.ok(message);
-    }
+   public ResponseEntity<String> uploadFile(@RequestBody String xmlData) {
+       // try {
+       // Création d'un XmlMapper pour lire le fichier XML
+       //    XmlMapper xmlMapper = new XmlMapper();
+       //    // Conversion du fichier XML en arbre JsonNode
+       //    JsonNode node = xmlMapper.readTree(file.getInputStream());
+
+       //    // Création d'un ObjectMapper pour écrire l'arbre JsonNode en tant que JSON
+       //    ObjectMapper objectMapper = new ObjectMapper();
+       //    String json = objectMapper.writeValueAsString(node);
+       kafkaSender.send("topic1", xmlData);
+       log.info(xmlData);
+       // Retourne le JSON résultant
+
+       return ResponseEntity.ok("json");
+       //    } catch (IOException e) {
+       //        e.printStackTrace();
+       //        return ResponseEntity.status(500).body("Erreur lors de la conversion du fichier XML en JSON");
+       //    }
+   }
 }
