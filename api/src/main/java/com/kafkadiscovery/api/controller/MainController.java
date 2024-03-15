f191d4dcd6f7d44ab09f2a49078de7ba4bf2774b
Robin JOSEPH <robin.joseph@decathlon.com>
2024-03-15 11:34:49 +0100

    Update API to process XML file and adjust Docker setup
    This commit introduces changes to the API controller to process a XML file instead of a string. The XML file is converted to a JSON string and then sent to a Kafka topic. In addition, this update revises the Docker compose set up removing two volumes related to "mirth-data" and "output-files'. The new dependency "jackson-dataformat-xml" also has been added to the api/pom.xml for XML processing.

--- api/src/main/java/com/kafkadiscovery/api/controller/MainController.java
+++ api/src/main/java/com/kafkadiscovery/api/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.kafkadiscovery.api.controller;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.kafkadiscovery.api.service.KafkaSender;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -18,24 +24,22 @@ public class MainController {
     private final KafkaSender kafkaSender;
      
    @PostMapping("/data")
-   public ResponseEntity<String> uploadFile(@RequestBody String xmlData) {
-       // try {
-       // Création d'un XmlMapper pour lire le fichier XML
-       //    XmlMapper xmlMapper = new XmlMapper();
-       //    // Conversion du fichier XML en arbre JsonNode
-       //    JsonNode node = xmlMapper.readTree(file.getInputStream());
+   public ResponseEntity<String> uploadFile(@RequestBody MultipartFile xmlData) {
+        try {
+            XmlMapper xmlMapper = new XmlMapper();
+            JsonNode node = xmlMapper.readTree(xmlData.getInputStream());
 
-       //    // Création d'un ObjectMapper pour écrire l'arbre JsonNode en tant que JSON
-       //    ObjectMapper objectMapper = new ObjectMapper();
-       //    String json = objectMapper.writeValueAsString(node);
-       kafkaSender.send("topic1", xmlData);
-       log.info(xmlData);
-       // Retourne le JSON résultant
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(node);
+            kafkaSender.send("topic1", json);
+            log.info(json);
 
-       return ResponseEntity.ok("json");
-       //    } catch (IOException e) {
-       //        e.printStackTrace();
-       //        return ResponseEntity.status(500).body("Erreur lors de la conversion du fichier XML en JSON");
-       //    }
-   }
+            return ResponseEntity.ok(json);
+
+       } catch (IOException e) {
+           log.error("Erreur lors de la conversion du fichier XML en JSON", e);
+            return ResponseEntity.status(500).body("Erreur lors de la conversion du fichier XML en JSON");
+       }
+    }
 }
+
