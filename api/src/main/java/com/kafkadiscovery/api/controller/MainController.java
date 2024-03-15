f5b7e3a95b304fd5d1e9e929989061b464b99374
MathieuSymphorien <mathieu.symphorien@etu.univ-amu.fr>
2024-03-15 14:02:31 +0100

    firts commit

--- api/src/main/java/com/kafkadiscovery/api/controller/MainController.java
+++ api/src/main/java/com/kafkadiscovery/api/controller/MainController.java
@@ -24,16 +24,45 @@ public class MainController {
 
    @PostMapping("/data")
    public ResponseEntity<String> uploadFile(@RequestBody String xmlData) {
-       log.info("Received XML data: " + xmlData);
         try {
             XmlMapper xmlMapper = new XmlMapper();
             JsonNode node = xmlMapper.readTree(xmlData);
+            JsonNode rootNode = mapper.readTree(json);
 
+           
+            // Ajoutez la logique pour PV1 si nécessaire
             ObjectMapper objectMapper = new ObjectMapper();
             String json = objectMapper.writeValueAsString(node);
-            kafkaSender.send("topic1", json);
-            log.info(json);
+            kafkaSender.send("topic1", json); 
+            
+            JsonNode mshNode = rootNode.path("MSH");
+            JsonNode pidNode = rootNode.path("PID");
+            // Extrait les données MSH
+            String msh9 = mshNode.path("MSH.9").path("MSH.9.1").asText();
+    
+            // Extrait les données PID
+            String pid3 = pidNode.path("PID.3").path("PID.3.1").asText();
+            String patientName = pidNode.path("PID.5").get(0).path("PID.5.1").asText();
+            String patientSurname = pidNode.path("PID.5").get(0).path("PID.5.2").asText();
+            // log.info(json);
+            log.info("-----------------");
+            log.info(" ");
+            // Afficher les résultats
+            log.info("MSH-9: " + msh9);
+            log.info("PID-3 (Patient ID): " + pid3);
+            log.info("Patient Name: " + patientName);
+            log.info("Patient Surname: " + patientSurname);
 
+            
+            
+            // JsonNode pidNode = node.get("PID");
+            // JsonNode personNode = pidNode.get("PID.5").get(0);
+            // JsonNode addressNode = pidNode.get("PID.11").get(0);
+            
+            log.info(personNode);
+            log.info(addressNode);
+            log.info(" ");
+            log.info("--------------------");
             return ResponseEntity.ok(json);
 
        } catch (IOException e) {
