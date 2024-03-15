236d9fda248216d0240190aecdb16af23ca3f442
MathieuSymphorien <mathieu.symphorien@etu.univ-amu.fr>
2024-03-15 14:02:31 +0100

    firts commit

---
+++ user/src/Producteur.java
@@ -0,0 +1,27 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class Producteur {
+    private final KafkaProducer<String, String> producer;
+    private final String topic;
+
+    public KafkaCommandProducer(String servers, String topic) {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        this.producer = new KafkaProducer<>(properties);
+        this.topic = topic;
+    }
+
+    public void sendCommand(String command) {
+        producer.send(new ProducerRecord<>(topic, command));
+    }
+
+    public void close() {
+        producer.close();
+    }
+}
